424ea8a1328f858d46a10005db4cf7bcb31d7375
Deephang Thegim <132187755+ninjaaaxxx@users.noreply.github.com>
2024-02-14 15:25:40 +0545

    Update otpscreen.dart

--- lib/otpscreen.dart
+++ lib/otpscreen.dart
@@ -1,49 +1,102 @@
-import 'dart:io';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:sajhabackup/Cart/cartmodel.dart';
-import 'package:sajhabackup/Splashes/splashscreen.dart';
-import 'package:sajhabackup/themes/themeprovider.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:sajhabackup/EasyConst/Colors.dart';
+import 'package:sajhabackup/EasyConst/Styles.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid
-      ? await Firebase.initializeApp(
-          options: const FirebaseOptions(
-              apiKey: 'AIzaSyB22xWy99xjXerlI-c-sFvShkTrUlFs3nc',
-              appId: '1:198996371343:android:508c7cd18165d39e404dfa',
-              messagingSenderId: '198996371343',
-              projectId: 'sajhabookstore',
-              storageBucket: 'gs://sajhabookstore.appspot.com'))
-      : await Firebase.initializeApp();
-  runApp(Home());
+class OTPScreen extends StatefulWidget {
+  final String vid;
+  final void Function(String code)
+      registerFunction; // Function to register user
+
+  const OTPScreen({Key? key, required this.vid, required this.registerFunction})
+      : super(key: key);
+
+  @override
+  _OTPScreenState createState() => _OTPScreenState();
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class _OTPScreenState extends State<OTPScreen> {
+  var code = "";
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => CartModel()),
-        ChangeNotifierProvider(create: (context) => ThemeProvider()),
-      ],
-      child: MyApp(),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('assets/images/otp.png', height: 200, width: 200),
+              Center(
+                child: Text(
+                  "OTP Verification",
+                  style: TextStyle(fontSize: 39),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 6),
+                child: Text(
+                  "Enter OTP",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: regular,
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              textCode(),
+              SizedBox(height: 80),
+              button(),
+            ],
+          ),
+        ),
+      ),
     );
   }
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  Widget button() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          widget.registerFunction(code); // Call register function with OTP code
+        },
+        style: ElevatedButton.styleFrom(
+          primary: color,
+          padding: const EdgeInsets.all(16.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 80),
+          child: Text(
+            'Verify & Proceed',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: Provider.of<ThemeProvider>(context).themeData,
-      home: splashscreen(),
+  Widget textCode() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: PinCodeTextField(
+          appContext: context,
+          length: 6,
+          onChanged: (value) {
+            setState(() {
+              code = value;
+            });
+          },
+          // Other properties you may need to customize the appearance
+        ),
+      ),
     );
   }
 }
