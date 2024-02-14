34c6431ce180ef3cd4d55cc8aff3a612c5236c3f
Deephang Thegim <132187755+ninjaaaxxx@users.noreply.github.com>
2024-02-14 14:45:00 +0545

    Update otpscreen.dart

--- lib/otpscreen.dart
+++ lib/otpscreen.dart
@@ -1,1 +1,49 @@
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sajhabackup/Cart/cartmodel.dart';
+import 'package:sajhabackup/Splashes/splashscreen.dart';
+import 'package:sajhabackup/themes/themeprovider.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyB22xWy99xjXerlI-c-sFvShkTrUlFs3nc',
+              appId: '1:198996371343:android:508c7cd18165d39e404dfa',
+              messagingSenderId: '198996371343',
+              projectId: 'sajhabookstore',
+              storageBucket: 'gs://sajhabookstore.appspot.com'))
+      : await Firebase.initializeApp();
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartModel()),
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ],
+      child: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      home: splashscreen(),
+    );
+  }
+}
